eb63ac2fd2b26150ee3d16aea20265689b39745c
Altieres de Matos <altitdb@gmail.com>
2016-10-19 21:41:10 -0200

    Finishing Mapping ORM

--- events/src/main/java/com/ondetemjogo/model/Event.java
+++ events/src/main/java/com/ondetemjogo/model/Event.java
@@ -2,12 +2,44 @@ package com.ondetemjogo.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
 public class Event {
 
+	@Id
+	@SequenceGenerator(name = "SEQ_EVENT", sequenceName = "GEN_EVENT_ID", allocationSize = 1)
+	@GeneratedValue(generator = "SEQ_EVENT", strategy = GenerationType.SEQUENCE)
+	private Long idEvent;
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(nullable = false)
 	private Date date;
+	@ManyToOne
+	@JoinColumn(name = "id_establishment", nullable = false)
 	private Establishment establishment;
+	@ManyToOne
+	@JoinColumn(name = "id_house_team", nullable = false)
 	private Team houseTeam;
+	@ManyToOne
+	@JoinColumn(name = "id_visit_team", nullable = false)
 	private Team visitTeam;
+
+	public Long getIdEvent() {
+		return idEvent;
+	}
+
+	public void setIdEvent(Long idEvent) {
+		this.idEvent = idEvent;
+	}
 
 	public Date getDate() {
 		return date;
