c30ec989a50d2ea258b8ca70907bd2037800f272
Altieres de Matos <altitdb@gmail.com>
2016-10-22 14:00:02 -0200

    Updating Event Search in Repositories

--- events/src/main/java/com/ondetemjogo/repository/EventRepository.java
+++ events/src/main/java/com/ondetemjogo/repository/EventRepository.java
@@ -1,14 +1,10 @@
 package com.ondetemjogo.repository;
 
-import java.util.Date;
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.ondetemjogo.model.Event;
 
-public interface EventRepository extends JpaRepository<Event, Long> {
-
-	List<Event> findAllByDateGreaterThanEqual(Date date);
+public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecificationExecutor<Event> {
 
 }
