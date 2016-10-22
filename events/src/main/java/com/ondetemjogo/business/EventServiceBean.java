eab3cfa2d7387dbe3433ade124ab9e4cc6685326
Altieres de Matos <altitdb@gmail.com>
2016-10-22 14:00:24 -0200

    Updating EventServiceBean

--- events/src/main/java/com/ondetemjogo/business/EventServiceBean.java
+++ events/src/main/java/com/ondetemjogo/business/EventServiceBean.java
@@ -5,10 +5,12 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.ondetemjogo.model.Event;
 import com.ondetemjogo.repository.EventRepository;
+import com.ondetemjogo.specification.EventSpecification;
 
 @Service
 public class EventServiceBean implements EventService {
@@ -19,12 +21,13 @@ public class EventServiceBean implements EventService {
 	@Override
 	public List<Event> getEvents(String search) {
 		Calendar calendar = Calendar.getInstance();
-		calendar.set(Calendar.HOUR, 0);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
 		calendar.set(Calendar.MINUTE, 0);
 		calendar.set(Calendar.SECOND, 0);
 		calendar.set(Calendar.MILLISECOND, 0);
 		Date date = calendar.getTime();
-		return eventRepository.findAllByDateGreaterThanEqual(date);
+		Specification<Event> spec = EventSpecification.findEvents(date, search);
+		return eventRepository.findAll(spec);
 	}
 		
 }
