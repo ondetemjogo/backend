bf6104cd0c306699237d60a4bdbacaa53bd3057b
Altieres de Matos <altitdb@gmail.com>
2016-11-02 19:29:16 -0200

    Updating cors config

--- events/src/main/java/com/ondetemjogo/EventsApplication.java
+++ events/src/main/java/com/ondetemjogo/EventsApplication.java
@@ -2,12 +2,30 @@ package com.ondetemjogo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication(scanBasePackages = "com.ondetemjogo")
 public class EventsApplication {
 
    public static void main(String[] args) {
       SpringApplication.run(EventsApplication.class, args);
+   }
+
+   @Bean
+   public WebMvcConfigurer corsConfigurer() {
+       return new WebMvcConfigurerAdapter() {
+           @Override
+           public void addCorsMappings(CorsRegistry registry) {
+        	   String[] permitedUrls = new String[3];
+        	   permitedUrls[0] = "https://ondetemjogo.herokuapp.com";
+        	   permitedUrls[1] = "http://ondetemjogo.herokuapp.com";  
+        	   permitedUrls[2] = "http://localhost:8080";  
+               registry.addMapping("/api/v1/**").allowedOrigins(permitedUrls);
+           }
+       };
    }
 
 }
